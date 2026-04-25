9bcce8ed54a58816388eb2f62862ed947ed6d8c8
Dhanush <mc.dhanush.msd@gmail.com>
2026-04-25 15:35:38 +0000

    CLEANUP1

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ from pydantic import BaseModel, field_validator
 from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
-from typing import Optional, TypedDict, NotRequired
+from typing import Optional
+from typing_extensions import TypedDict, NotRequired
 import os, json, re, uuid, math
 
 import logging
